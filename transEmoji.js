2b9e2340784eafb0b1a293cfe9da54caac0dc154
meornmee <meronmee@163.com>
2016-10-28 15:45:41 +0800

    Signed-off-by: meornmee <meronmee@163.com>

--- transEmoji.js
+++
@@ -1,7 +0,0 @@
-function transEmoji(str, pattern){
-  if(!pattern){
-	  pattern = /\[e\]\s*(.+?)\s*\[\/e\]/gi;
-  }
-  str = '' +str;
-  return str.replace(pattern, '<span class="emoji emoji-$1"></span>').replace(/\[\s*([0-9a-f]+?)\s*\]/gi, '<span class="emoji emoji-$1"></span>');            
-}
